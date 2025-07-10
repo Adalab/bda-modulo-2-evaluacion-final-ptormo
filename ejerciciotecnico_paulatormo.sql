761d1489380f3e3f0015e80902f3d171e86e03a0
ptormo <paulatormoa@gmail.com>
2025-07-10 14:21:07 +0200

    ejercicio tecnico Paula Tormo

--- ejerciciotecnico_paulatormo.sql
+++ ejerciciotecnico_paulatormo.sql
@@ -8,15 +8,17 @@ SELECT f.title
 FROM film AS f
 WHERE rating = "PG-13";
 -- 3.Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción.
-SELECT *
-FROM film;
+-- query final
 SELECT f.title, f.description
 FROM film AS f
 WHERE f.description LIKE '%amazing%'; -- todas las películas las devuelve conteniendo la palabra
+-- query control
+SELECT *
+FROM film;
 -- 4. Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos.
 SELECT f.title
 FROM film AS f
-WHERE length > 120;
+WHERE f.length > 120;
 -- 5. Recupera los nombres de todos los actores.
 SELECT CONCAT(a.first_name, ' ', a.last_name) AS nombre
 FROM actor AS a;
@@ -44,14 +46,14 @@ GROUP BY f.rating;
  FROM customer AS c
  INNER JOIN rental AS r -- todas la peliculas tienen que tener un cliente
  ON c.customer_id = r.customer_id -- unimos las tablas de cliente y alquiler
- GROUP BY c.customer_id, nombre -- agrupamos por cada cliente
+ GROUP BY c.customer_id, c.first_name, c.last_name -- agrupamos por cada cliente
  HAVING COUNT(r.rental_id) > 0; -- contamos por cada cliente las peliculas que ha alquilado, siendo este nº > 0, tiene que alquilar algo
  -- query entendiendo que nos devuelva todos los clientes hayan o no alquilado
  SELECT c.customer_id, CONCAT(c.first_name, ' ', c.last_name) AS nombre, COUNT(rental_id) AS cantidad_peliculas_alquiladas
  FROM customer AS c
  LEFT JOIN rental AS r -- nos une a todos los clientes, hayan o no alquilado
  ON c.customer_id = r.customer_id -- unimos las tablas de cliente y alquiler
- GROUP BY c.customer_id, nombre; -- agrupamos por cada cliente;
+ GROUP BY c.customer_id, c.first_name, c.last_name; -- agrupamos por cada cliente;
  
  -- comprobación de si hay algún cliente sin alquiler: lo devuelve vacío
 SELECT c.customer_id, CONCAT(c.first_name, ' ', c.last_name) AS nombre
@@ -80,7 +82,7 @@ HAVING COUNT(r.rental_id) > 0; -- contamos el alquiler de peliculas de cada cate
 SELECT AVG(length) AS promedio
 FROM film;
 -- query final
-SELECT DISTINCT f.rating, AVG(length) AS promedio
+SELECT f.rating, AVG(length) AS promedio
 FROM film AS f
 GROUP BY f.rating; -- agrupamos por su clasificación, y luego el promedio en el select
 
@@ -97,10 +99,22 @@ GROUP BY f.rating; -- agrupamos por su clasificación, y luego el promedio en el
  SELECT f.title, f.description
  FROM film AS f
  WHERE f.description LIKE '% dog %'  OR f.description LIKE '% cat %'; -- contienen la palabra dog o cat como palabras individuales, patrón
+ -- query opcion 2
+  SELECT f.title, f.description
+ FROM film AS f
+ WHERE f.description LIKE '% dog %'
+ UNION 
+ SELECT f.title, f.description
+ FROM film AS f
+ WHERE f.description LIKE '% CAT %';
  -- query funciona dog
  SELECT f.title, f.description
  FROM film AS f
- WHERE f.description LIKE '% dog %'; 
+ WHERE f.description LIKE '% dog %'
+ 
+ SELECT f.title, f.description
+ FROM film AS f
+ WHERE f.description LIKE '% CAT %'; 
  
  -- query para ver todo de film
  SELECT *
@@ -147,7 +161,7 @@ GROUP BY f.rating; -- agrupamos por su clasificación, y luego el promedio en el
  INNER JOIN film_actor AS fa -- tabla intermedia, solo quiero actores que tengan coincidencias con actores en peliculas
  ON a.actor_id = fa.actor_id
  GROUP BY a.actor_id, a.first_name, a.last_name -- unimos por los nombres de columna, actor_id es pk y único= actores individuales pese a nombres duplicados
-HAVING COUNT(fa.film_id) > 10; -- film_id es único
+HAVING COUNT(fa.film_id) > 10; -- film_id es único, hace referencia a las peliculas
  
  
  -- query control con num_pelis
@@ -159,15 +173,158 @@ HAVING COUNT(fa.film_id) > 10; -- film_id es único
 HAVING COUNT(fa.film_id) > 10; -- queremos actores que aparezca en +10 pelis, diferente titulo =diferente peli
  
 -- 19 Encuentra el título de todas las películas que son "R" y tienen una duración mayor a 2 horas en la tabla film
--- queries control
-SELECT f.length
+-- query final
+SELECT f.title
 FROM film AS f
-WHERE length > 
+WHERE f.rating = "R" AND f.length > 120 -- su clasificacion es r y su duracion es de + 2h =120min
+ORDER BY f.length ASC;
+-- queries control
+SELECT f.length 
+FROM film AS f
+WHERE f.length > 120
+ORDER BY f.length ASC;
 SELECT f.title, f.rating
 FROM film AS f
 WHERE rating = "R";
 SELECT *
 FROM film;
- -- Encuentra las categorías de películas que tienen un promedio de duración superior a 120 minutos y
+ -- 20 Encuentra las categorías de películas que tienen un promedio de duración superior a 120 minutos y
 --  muestra el nombre de la categoría junto con el promedio de duración
+
+SELECT c.name, AVG(f.length) AS promedio
+FROM category AS c
+INNER JOIN film_category AS fc
+ON c.category_id = fc.category_id -- unimos todas las coincidencias de categorias con peliculas/tabla intermedia
+INNER JOIN film AS f
+ON fc.film_id = f.film_id
+GROUP BY c.name -- agrupamos por nombre de categoria
+HAVING AVG(f.length) > 120; -- donde el promedio de la duracion de cada pelicula sea mayor a 120,no inclusive
+
+-- query control
+SELECT film.title, AVG(film.length) as promedio
+FROM film
+GROUP BY title
+HAVING AVG(length) > 120;
+ -- 21  Encuentra los actores que han actuado en al menos 5 películas y 
+ -- muestra el nombre del actor junto con la cantidad de películas en las que han actuado.
+ SELECT CONCAT(a.first_name, ' ', a.last_name) AS nombre, COUNT(fa.film_id) AS cantidad
+ FROM actor AS a
+ INNER JOIN film_actor AS fa -- unimos actor con actores en peliculas
+ ON a.actor_id = fa.actor_id
+ GROUP BY a.actor_id, a.first_name, a.last_name -- agrupamos por nombre y actor_id que es único para cada actor
+ HAVING COUNT(fa.film_id) >= 5; -- al menos cinco, inclusive 5, contamos por id que es unico = 1 pelicula
+ 
+ -- 22 Encuentra el título de todas las películas que fueron alquiladas por más de 5 días. 
+ -- Utiliza una  subconsulta para encontrar los rental_ids con una duración superior a 5 días y
+ -- luego selecciona las películas correspondientes.
+ -- query final
+SELECT DISTINCT f.title --  nos devuelve el nombre de las peliculas 1 vez, de alquiler superior a 5 días, no inclusive
+ FROM film AS f
+ INNER JOIN inventory AS i
+ ON f.film_id = i.film_id -- unimos el titulo de film con rental, a través de tabla inventory
+ INNER JOIN rental AS r
+ ON i.inventory_id = r.inventory_id
+ WHERE r.rental_id IN (SELECT rental_id -- quiero los rental id que estén dentro (IN) de los rental_id que tuvieron una fecha de devolucion > a 5 días
+						FROM rental
+                        WHERE DATEDIFF(return_date, rental_date) > 5); -- subconsulta que nos devuelve alquiler > 5 dias
+ 
+ -- queries comprobaciones
+ SELECT *
+ FROM film;
+ SELECT * FROM inventory;
+ SELECT * FROM rental;
+ SELECT f.title
+ FROM film AS f
+ INNER JOIN inventory AS i
+ ON f.film_id = i.film_id -- unimos el titulo de film con rental, a través de tabla inventory
+ INNER JOIN rental AS r
+ ON i.inventory_id = r.inventory_id;
+ -- subconsulta que nos devuelve alquiler > 5 dias
+-- query consulta
+
+SELECT * FROM film;
+SELECT f.title, DATEDIFF(return_date, rental_date) AS duracion -- nos devuelve mas de 5 dias
+ FROM film AS f
+ INNER JOIN inventory AS i
+ ON f.film_id = i.film_id -- unimos el titulo de film con rental, a través de tabla inventory
+ INNER JOIN rental AS r
+ ON i.inventory_id = r.inventory_id
+ WHERE r.rental_id IN (SELECT rental_id -- quiero los rental id que estén dentro (IN) de los rental_id que tuvieron una fecha de devolucion > a 5 días
+						FROM rental
+                        WHERE DATEDIFF(return_date, rental_date) > 5); -- subconsulta que nos devuelve alquiler > 5 dias
+SELECT f.title, DATEDIFF(return_date, rental_date) AS duracion -- devuelve titulos repetidos: probar distinct
+ FROM film AS f
+ INNER JOIN inventory AS i
+ ON f.film_id = i.film_id -- unimos el titulo de film con rental, a través de tabla inventory
+ INNER JOIN rental AS r
+ ON i.inventory_id = r.inventory_id
+ WHERE r.rental_id IN (SELECT rental_id -- quiero los rental id que estén dentro (IN) de los rental_id que tuvieron una fecha de devolucion > a 5 días
+						FROM rental
+                        WHERE DATEDIFF(return_date, rental_date) > 5); -- subconsulta que nos devuelve alquiler > 5 dias
+ 
+ SELECT DATEDIFF(return_date, rental_date) AS duracion, rental_id -- para calcular la diferencia entre dos fechas usamos datediff(col1,col2)
+ FROM rental 
+ WHERE DATEDIFF(return_date, rental_date) > 5
+ ;
+ SELECT *
+ FROM rental; -- quiero restar returndate a rentaldate y que eso sea mayor a 5
+
+ -- rental une con inventory por inventory_id, que une con film title por film_id
+ 
+ -- 23 Encuentra el nombre y apellido de los actores que no han actuado en ninguna película de la categoría "Horror". 
+ -- Utiliza una subconsulta para encontrar los actores que han actuado en películas de la categoría "Horror" y luego exclúyelos de la lista de actores.
+ -- query control
+ 
+
+SELECT a.first_name, a.last_name -- quiero que me devuelva el nombre de los que NOT IN mi subconsulta
+FROM actor AS a
+WHERE a.actor_id NOT IN (SELECT a.first_name, a.last_name -- nos devuelve actores que salen en horror
+ FROM actor AS a
+ INNER JOIN film_actor AS fa
+ ON a.actor_id = fa.actor_id
+ INNER JOIN film AS f
+ ON fa.film_id = f.film_id
+ INNER JOIN film_category AS fc
+ On f.film_id = fc.film_id
+ INNER JOIN category AS c
+ ON fc.category_id = c.category_id
+ WHERE c.name = "Horror");
+ 
+ SELECT a.first_name, a.last_name, c.name -- nos devuelve actores que salen en horror
+ FROM actor AS a
+ INNER JOIN film_actor AS fa
+ ON a.actor_id = fa.actor_id
+ INNER JOIN film AS f
+ ON fa.film_id = f.film_id
+ INNER JOIN film_category AS fc
+ On f.film_id = fc.film_id
+ INNER JOIN category AS c
+ ON fc.category_id = c.category_id
+ WHERE c.name = "Horror";
+ 
+ -- 24 Encuentra el título de las películas que son comedias y tienen una duración mayor a 180 minutos en la tabla film
+ -- query final
+  SELECT f.title
+ FROM film AS f
+ INNER JOIN film_category AS fc
+ ON f.film_id = fc.film_id
+ INNER JOIN category AS c
+ ON fc.category_id = c.category_id
+ WHERE c.name = 'Comedy' AND f.length > 180;
+ 
+  -- query consulta
+ SELECT f.title, f.length, c.name
+ FROM film AS f
+ INNER JOIN film_category AS fc
+ ON f.film_id = fc.film_id
+ INNER JOIN category AS c
+ ON fc.category_id = c.category_id
+ WHERE c.name = 'Comedy' AND f.length > 180;
+ 
+ SELECT * FROM category 
+ WHERE category.name = "Comedy";
+ SELECT *
+ FROM film_category
+ WHERE category_id = "5";
+ 
  
